2baf58d0ea359e0cb91ddbf1a5c802a31e3ae020
Natan Vieira <itroken99@gmail.com>
2022-10-30 23:59:40 -0300

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
 use clap::{Command, Parser, Subcommand};
+use std::{fmt::format, fs::File, io::Write, process::Command as StdCommand, sync::Arc};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 #[clap(propagate_version = true)]
 struct Cli {
     #[clap(subcommand)]
-    command: Commands
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
     Install {
         #[clap(value_parser)]
-        product: Option<String>
-    }
+        product: Option<String>,
+    },
 }
 
-fn main() {
-    let cli = Cli::parse();
-   
-    match &cli.command {
-        Commands::Install { product } => {
-            println!("ainda n me chame de bb {:?}", product);
-        }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let contents =
+        reqwest::get("https://raw.githubusercontent.com/KatanPanel/cli/main/scripts/install_ui")
+            .await?
+            .text()
+            .await?;
+
+    {
+        let mut file = File::create("./pirocadefoice")?;
+        writeln!(file, "{}", contents)?;
     }
+
+    let command_result = StdCommand::new(format!("./{}", "pirocadefoice"))
+        .output()
+        .unwrap();
+
+    dbg!(command_result);
+    Ok(())
 }
---
+++ src/util.rs
@@ -0,0 +1,16 @@
+struct AuthError {
+    pub code: int32,
+    pub message: String,
+}
+
+pub struct AuthRequest<'a> {
+    api_token: &'a str,
+}
+
+impl<'a> AuthenticatedRequest<'a> {
+
+    pub fn new(api_token: &'a str, ) -> Self {
+        Self { api_token }
+    }
+
+}
